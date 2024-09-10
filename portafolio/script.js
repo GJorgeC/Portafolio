5becc11a17fb6d1e6bab8f7d6c1bb917f72aa66c
Jorge Garcia Carpio <garciajcarpio@gmail.com>
2024-09-10 20:52:00 +0200

    commit 10/9/24

--- portafolio/script.js
+++ portafolio/script.js
@@ -1,14 +1,18 @@
-document.getElementById('clickButton').addEventListener('click', function() {
-    const mainInfo = document.getElementById('main-info');
-    const mainContainer = document.getElementById('main-container');
-  
-    mainInfo.style.transform = 'translateY(-100%)';
-    mainInfo.style.opacity = '0';
-  
-    setTimeout(() => {
-      mainContainer.style.transform = 'translateY(0)';
-      mainContainer.style.opacity = '1';
-    }, 500); // Ajusta este valor al tiempo de la transición en CSS (0.5s = 500ms)
+document.addEventListener('DOMContentLoaded', function() {
+  const clickButton = document.getElementById('clickButton');
+  const mainInfo = document.getElementById('main-info');
+  const mainContainer = document.getElementById('main-container');
+
+  clickButton.addEventListener('click', function() {
+      mainInfo.style.transform = 'translateY(-100%)';
+      mainInfo.style.opacity = '0';
+
+      setTimeout(() => {
+          mainContainer.style.transform = 'translateY(0)';
+          mainContainer.style.opacity = '1';
+
+          // Redirige a una página específica después de la animación
+          window.location.href = 'sobre_mi.html'; // Cambia esto por la URL a la que quieres redirigir
+      }, 500); // Ajusta este valor al tiempo de la transición en CSS (0.5s = 500ms)
   });
-  
-  
+});
